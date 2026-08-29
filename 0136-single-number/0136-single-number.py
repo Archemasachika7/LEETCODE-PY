efc1f03e2d5db6613a5ec55decc20e487b5ed509
Archishman Das <202021058+Archemasachika7@users.noreply.github.com>
2026-08-29 18:58:43 +0530

    Time: 8 ms (20.62%), Space: 21.6 MB (18.98%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen = []
+        d= {}
         n=len(nums)
         duplicate = True
         for i in range(n):
-            if nums.count(nums[i]) == 1 :
-                return nums[i]
+            if nums[i] not in d.keys() :
+                d[nums[i]] = 1
+            else :
+                d[nums[i]] =d[nums[i]] + 1
+        for key in d.keys():
+            if d[key] == 1 :
+                return key            
+
                    
             
         
